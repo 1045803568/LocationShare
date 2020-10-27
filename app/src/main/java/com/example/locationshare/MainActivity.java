eacf5d537a75673ecd00788b8fee19714307fbac
Quhong <1045803568@qq.com>
2020-10-27 21:11:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/locationshare/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.locationshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap = mapView.getMap();
+    }
+}
